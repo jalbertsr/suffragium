c2e96abbc84d6003205ce2a681a71ab6d6ac7d6c
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-08-23 16:50:46 +0200

    heroku changes

--- Server/app.js
+++ Server/app.js
@@ -1,7 +1,6 @@
 const express = require('express')
 const path = require('path')
 // const mongoose = require('mongoose')
-const routes = require('./routes/')
 const app = express()
 
 const pathPublic = path.join(process.cwd(), 'client')
@@ -13,8 +12,6 @@ app.use(express.static(pathPublic))
 
 // mongoose.Promise = Promise
 // mongoose.connect(urlDb, { useMongoClient: true })
-
-app.use(routes)
 
 app.listen(PORT)
 console.log(`Listening on PORT ${PORT}`)
