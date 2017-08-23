fc754a4cc5ad3727a63363d98f5609f3b75d726f
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-08-23 15:39:13 +0200

    saving before pug home chnage

--- Server/app.js
+++ Server/app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const path = require('path')
 // const mongoose = require('mongoose')
+const routes = require('./routes/')
 const app = express()
 
 const pathPublic = path.join(process.cwd(), 'client')
@@ -12,6 +13,8 @@ app.use(express.static(pathPublic))
 
 // mongoose.Promise = Promise
 // mongoose.connect(urlDb, { useMongoClient: true })
+
+app.use(routes)
 
 app.listen(PORT)
 console.log(`Listening on PORT ${PORT}`)
