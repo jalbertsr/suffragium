4f9a0ad9bbe3aca3bdf49cb5250c4fbf339ba316
Joan Albert Segura <jalbertsr@protonmail.ch>
2017-08-24 20:51:57 +0200

    added blob & saveFile libs for chart image clint download

--- client/app/routes/results/controller.js
+++ client/app/routes/results/controller.js
@@ -1,4 +1,4 @@
-/* global angular */
+/* global angular Chart saveAs */
 
 (function () {
   'use strict'
@@ -7,7 +7,15 @@
   app.controller('resultsController', resultsController)
 
   function resultsController ($scope, $rootScope) {
-    var myChart
+    let myChart
+    let saveCount = 0
+
+    $scope.saveChart = function () {
+      document.getElementById('myChart').toBlob(function (blob) {
+        saveCount++
+        saveAs(blob, `chart_${saveCount}.png`)
+      })
+    }
 
     $scope.changeChart = function (chartType) {
       const ctx = document.getElementById('myChart')
@@ -19,21 +27,21 @@
         case 'horizontalBar':
         case 'bar':
           let data = {
-            labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+            labels: ['Blue', 'Red', 'Yellow', 'Green', 'Purple', 'Orange'],
             datasets: [{
               label: '# of Votes',
               data: [12, 19, 3, 5, 2, 3],
               backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
                 'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 99, 132, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
                 'rgba(75, 192, 192, 0.2)',
                 'rgba(153, 102, 255, 0.2)',
                 'rgba(255, 159, 64, 0.2)'
               ],
               borderColor: [
-                'rgba(255,99,132,1)',
                 'rgba(54, 162, 235, 1)',
+                'rgba(255,99,132,1)',
                 'rgba(255, 206, 86, 1)',
                 'rgba(75, 192, 192, 1)',
                 'rgba(153, 102, 255, 1)',
@@ -53,7 +61,7 @@
             }
           }
 
-          myChart = new Chart (ctx, {
+          myChart = new Chart(ctx, {
             type: chartType,
             data: data,
             options: options
@@ -65,24 +73,34 @@
             datasets: [{
               data: [10, 20, 30],
               backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)'
+                'rgba(54, 162, 235, 0.3)',
+                'rgba(255, 99, 132, 0.3)',
+                'rgba(255, 206, 86, 0.3)'
+              ],
+              borderColor: [
+                'rgba(54, 162, 235, 1)',
+                'rgba(255,99,132,1)',
+                'rgba(255, 206, 86, 1)'
               ]
             }],
           // These labels appear in the legend and in the tooltips when hovering different arcs
             labels: [
+              'Blue',
               'Red',
-              'Yellow',
-              'Blue'
+              'Yellow'
             ]
           }
 
-          myChart = new Chart (ctx, {
+          myChart = new Chart(ctx, {
             type: chartType,
             data: pieData
           })
       }
+      // var a = setTimeout(function () {
+      //   const graphUrl = ctx.toDataURL()
+      //   window.myImg = graphUrl
+      //   console.log(graphUrl)
+      // }, 1000)
     }
 
     /* --------------- BAR CHART ---------------- */
@@ -124,32 +142,12 @@
         }
       }
 
-      myChart = new Chart (ctx, {
+      myChart = new Chart(ctx, {
         type: 'horizontalBar',
         data: data,
         options: options
       })
       clearTimeout(initialGraph)
     }, 200)
-
-    /* --------------- DOUGHNUT CHART ---------------- */
-    // data = {
-    //   datasets: [{
-    //     data: [10, 20, 30]
-    //   }],
-
-    //   // These labels appear in the legend and in the tooltips when hovering different arcs
-    //   labels: [
-    //     'Red',
-    //     'Yellow',
-    //     'Blue'
-    //   ]
-    // }
-
-    // var myDoughnutChart = new Chart(ctx, {
-    //   type: 'doughnut',
-    //   data: data,
-    //   options: options
-    // })
   }
 })()
